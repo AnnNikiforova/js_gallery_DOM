3feb5960e556364a975b94437e54b99d5367b3a4
Anna <annanicky90gmail.com>
2024-10-23 13:58:19 +0400

    make build

---
+++ index.7ffb291b.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("largeImg");document.getElementById("thumbs").addEventListener("click",function(t){t.preventDefault();var a,n=t.target;"IMG"===n.tagName&&(n=n.parentElement),"A"===n.tagName&&(a=n.getAttribute("href"),e.src=a)});
+//# sourceMappingURL=index.7ffb291b.js.map
